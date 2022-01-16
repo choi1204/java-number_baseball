d9522093a3d469ca1411ecf747d5d837eabebb89
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-01-16 16:31:31 +0900

    feat(baseball): 몇개의 숫자가 같은지 알 수 있는 기능 구현

--- baseball/src/domain/Game.java
+++ baseball/src/domain/Game.java
@@ -1,16 +1,19 @@
 package domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Game {
 
     public static void main(String[] args) {
 
-        NumberGenerator numberGenerator = new NumberGenerator();
-        List<Integer> player = numberGenerator.createRandomNumbers();
-        List<Integer> computer = numberGenerator.createRandomNumbers();
-        System.out.println("player = " + player);
-        System.out.println("computer = " + computer);
+//        NumberGenerator numberGenerator = new NumberGenerator();
+//        List<Integer> computer = numberGenerator.createRandomNumbers();
+//        System.out.println("computer = " + computer);
+
+        Judgment judgment = new Judgment();
+        int count = judgment.correctCount(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+        System.out.println(count);
     }
 
 }
--- baseball/src/domain/Judgment.java
+++ baseball/src/domain/Judgment.java
@@ -1,14 +1,22 @@
 package domain;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public class Judgment {
 
     public int correctCount(List<Integer> computer, List<Integer> player) {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < computer.size(); i ++) {
+            int playerNumber = computer.get(i);
+            if (computer.contains(playerNumber)) {
+                result++;
+            }
+        }
+        return result;
     }
 
-    public boolean hasPlace(int placeNumber, int number) {
+    public boolean hasPlace(List<Integer> computer, int placeNumber, int number) {
         return false;
     }
 }

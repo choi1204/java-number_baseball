13d9c69e9088853e9dcf8555da9e230eed745cd0
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-01-16 15:55:48 +0900

    feat)기능 구현 메소드 기초 틀 제작.

---
+++ baseball/src/domain/NumberGenerator.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.List;
+import java.util.Random;
+
+public class NumberGenerator {
+
+    public List<Integer> createRandomNumbers() {
+        return null;
+    }
+}
---
+++ baseball/src/domain/Referee.java
@@ -0,0 +1,9 @@
+package domain;
+
+import java.util.List;
+
+public class Referee {
+
+    public void compare(List<Integer> computer, List<Integer> player) {
+    }
+}

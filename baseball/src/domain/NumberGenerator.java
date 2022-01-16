e0ef614f0da5163cd33ceb2aec0904e25fe0a958
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-01-16 16:19:20 +0900

    feat(NumberGenerator) : create Method(createRandomNumbers)

--- baseball/src/domain/NumberGenerator.java
+++ baseball/src/domain/NumberGenerator.java
@@ -1,11 +1,18 @@
 package domain;
 
+import java.awt.print.Pageable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class NumberGenerator {
 
     public List<Integer> createRandomNumbers() {
-        return null;
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            int number = new Random().nextInt(8) + 1;
+            numbers.add(number);
+        }
+        return numbers;
     }
 }

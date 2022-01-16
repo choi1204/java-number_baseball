6d78988db3c0426890e440af5cddfe9331387bdb
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-01-16 16:09:19 +0900

    fixed untracked files

--- baseball/src/domain/NumberGenerator.java
+++ baseball/src/domain/NumberGenerator.java
@@ -1,11 +1,17 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class NumberGenerator {
 
     public List<Integer> createRandomNumbers() {
-        return null;
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            int number = new Random().nextInt(9) + 1;
+            numbers.add(number);
+        }
+        return numbers;
     }
 }
